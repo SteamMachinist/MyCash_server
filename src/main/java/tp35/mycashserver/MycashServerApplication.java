d8bbd9567739ec28d6c853e7edea1c0d58707b7c
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-05-20 22:43:50 +0300

    Удален лишний закомментированный код

--- src/main/java/tp35/mycashserver/MycashServerApplication.java
+++ src/main/java/tp35/mycashserver/MycashServerApplication.java
@@ -2,10 +2,8 @@ package tp35.mycashserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication//(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class MycashServerApplication {
 
 	public static void main(String[] args) {
