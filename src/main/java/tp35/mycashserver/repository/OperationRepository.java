a394537ae9452e223da62ce62c6e66b2db782f21
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-05-23 15:54:16 +0300

    Добавлен поиск операций по счету и датам операций

--- src/main/java/tp35/mycashserver/repository/OperationRepository.java
+++ src/main/java/tp35/mycashserver/repository/OperationRepository.java
@@ -1,7 +1,26 @@
 package tp35.mycashserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import tp35.mycashserver.entity.AccountEntity;
 import tp35.mycashserver.entity.OperationEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OperationRepository extends JpaRepository<OperationEntity, Long> {
+    List<OperationEntity> findAllByAccount(AccountEntity account);
+
+//    default List<OperationEntity> findAllByAccountAndYearMonth(AccountEntity account, LocalDateTime from, LocalDateTime to) {
+//        return findAllByAccountAndCreatedBetween(account, from, to);
+//    }
+//
+//    default List<OperationEntity> findAllByAccountAndYearMonthDate(AccountEntity account, LocalDateTime from, LocalDateTime to) {
+//        return
+//    }
+
+    List<OperationEntity> findAllByAccountAndCreatedBetween(AccountEntity account, LocalDateTime from, LocalDateTime to);
+
+    //    @Query("SELECT ALL from operation ")
+//    List<OperationEntity> findAllByAccountAndMonth();
 }
--- src/main/java/tp35/mycashserver/service/AccountService.java
+++ src/main/java/tp35/mycashserver/service/AccountService.java
@@ -3,16 +3,80 @@ package tp35.mycashserver.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tp35.mycashserver.mapper.AccountToEntityMapper;
+import tp35.mycashserver.mapper.OperationToEntityMapper;
+import tp35.mycashserver.mapper.UserToEntityMapper;
 import tp35.mycashserver.model.Account;
+import tp35.mycashserver.model.Operation;
+import tp35.mycashserver.model.User;
 import tp35.mycashserver.repository.AccountRepository;
+import tp35.mycashserver.repository.OperationRepository;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final OperationRepository operationRepository;
     private final AccountToEntityMapper accountToEntityMapper;
+    private final UserToEntityMapper userToEntityMapper;
+    private final OperationToEntityMapper operationToEntityMapper;
 
     public void add(Account account) {
         accountRepository.save(accountToEntityMapper.toAccountEntity(account));
+    }
+
+    public Account getAccountByOwnerAndName(User owner, String name) {
+        return accountToEntityMapper.toAccount(
+                accountRepository.findByOwnerAndName(userToEntityMapper.toUserEntity(owner), name));
+    }
+
+    public List<Operation> getOperationsOfAccount(Account account) {
+        return operationToEntityMapper.toOperationList(
+                operationRepository.findAllByAccount(accountToEntityMapper.toAccountEntity(account)));
+    }
+
+    public List<Operation> getOperationsOfAccountAndDate(Account account, int year, int month) {
+        LocalDateTime base = LocalDateTime.now();
+        LocalDateTime from = base.toLocalDate()
+                .withYear(year)
+                .withMonth(month)
+                .with(TemporalAdjusters.firstDayOfMonth())
+                .atTime(LocalTime.MIN);
+
+        LocalDateTime to = base.toLocalDate()
+                .withYear(year)
+                .withMonth(month)
+                .with(TemporalAdjusters.lastDayOfMonth())
+                .atTime(LocalTime.MAX).minus(1, ChronoUnit.SECONDS);
+
+        return getOperationsOfAccountBetween(account, from, to);
+    }
+
+    public List<Operation> getOperationsOfAccountAndDate(Account account, int year, int month, int day) {
+        LocalDateTime base = LocalDateTime.now();
+        LocalDateTime from = base.toLocalDate()
+                .withYear(year)
+                .withMonth(month)
+                .withDayOfMonth(day)
+                .atTime(LocalTime.MIN);
+
+        LocalDateTime to = base.toLocalDate()
+                .withYear(year)
+                .withMonth(month)
+                .withDayOfMonth(day)
+                .atTime(LocalTime.MAX).minus(1, ChronoUnit.SECONDS);
+
+        return getOperationsOfAccountBetween(account, from, to);
+    }
+
+    private List<Operation> getOperationsOfAccountBetween(Account account, LocalDateTime from, LocalDateTime to) {
+        return operationToEntityMapper.toOperationList(
+                operationRepository.findAllByAccountAndCreatedBetween(
+                        accountToEntityMapper.toAccountEntity(account), from, to));
     }
 }
