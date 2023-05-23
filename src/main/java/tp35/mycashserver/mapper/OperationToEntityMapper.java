1542dd56a7c491204781467550e6318b28255fa2
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-05-23 15:53:03 +0300

    Поправлен нейминг

--- src/main/java/tp35/mycashserver/mapper/OperationToEntityMapper.java
+++ src/main/java/tp35/mycashserver/mapper/OperationToEntityMapper.java
@@ -4,8 +4,15 @@ import org.mapstruct.Mapper;
 import tp35.mycashserver.entity.OperationEntity;
 import tp35.mycashserver.model.Operation;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface OperationToEntityMapper {
     OperationEntity toOperationEntity(Operation operation);
+
     Operation toOperation(OperationEntity operationEntity);
+
+    List<OperationEntity> toOperationEntityList(List<Operation> operationList);
+
+    List<Operation> toOperationList(List<OperationEntity> operationEntityList);
 }
