45d5c408c167fb855b6a36a0426255d6f6c05b26
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-05-23 15:52:37 +0300

    Добавлен поиск аккаунта по владельцу и названию

--- src/main/java/tp35/mycashserver/repository/AccountRepository.java
+++ src/main/java/tp35/mycashserver/repository/AccountRepository.java
@@ -2,5 +2,8 @@ package tp35.mycashserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tp35.mycashserver.entity.AccountEntity;
+import tp35.mycashserver.entity.UserEntity;
 
-public interface AccountRepository extends JpaRepository<AccountEntity, Long> {}
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+    AccountEntity findByOwnerAndName(UserEntity owner, String name);
+}
