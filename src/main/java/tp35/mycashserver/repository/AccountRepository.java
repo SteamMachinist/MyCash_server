378725f5b9f850d47445ad3dcb2c99d9faf5491d
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-01 00:45:01 +0300

    Добавлен метод репозитория нахождения всех счетов пользователя

--- src/main/java/tp35/mycashserver/repository/AccountRepository.java
+++ src/main/java/tp35/mycashserver/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tp35.mycashserver.entity.AccountEntity;
 import tp35.mycashserver.entity.UserEntity;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+    List<AccountEntity> findAllByOwner(UserEntity owner);
     AccountEntity findByOwnerAndName(UserEntity owner, String name);
 }
