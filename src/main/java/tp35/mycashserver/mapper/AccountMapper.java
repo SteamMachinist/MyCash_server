5874898ca194537a7a572b9cfbe404d0f740bf81
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-01 01:42:16 +0300

    Добавлен маппинг листов объеков

--- src/main/java/tp35/mycashserver/mapper/AccountMapper.java
+++ src/main/java/tp35/mycashserver/mapper/AccountMapper.java
@@ -5,6 +5,8 @@ import tp35.mycashserver.dto.AccountDTO;
 import tp35.mycashserver.entity.AccountEntity;
 import tp35.mycashserver.model.Account;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",
         uses = {UserMapper.class})
 public interface AccountMapper {
@@ -13,4 +15,13 @@ public interface AccountMapper {
     Account toAccount(AccountEntity accountEntity);
 
     AccountDTO toAccountDTO(Account account);
+
+    Account toAccount(AccountDTO accountDTO);
+
+
+    List<AccountEntity> toAccountEntities(List<Account> account);
+
+    List<Account> toAccounts(List<AccountEntity> accountEntities);
+
+    List<AccountDTO> toAccountDTOs(List<Account> account);
 }
