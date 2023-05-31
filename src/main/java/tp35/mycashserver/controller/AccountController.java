88e3c1aff8c634dcf6d522261b2727fb2506a021
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-01 00:45:46 +0300

    Реализован начальный crud контроллер счетов: get, getAll, add для пользователя

--- src/main/java/tp35/mycashserver/controller/AccountController.java
+++ src/main/java/tp35/mycashserver/controller/AccountController.java
@@ -1,7 +1,39 @@
 package tp35.mycashserver.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import tp35.mycashserver.dto.AccountDTO;
+import tp35.mycashserver.mapper.AccountMapper;
+import tp35.mycashserver.model.User;
+import tp35.mycashserver.service.AccountService;
+import tp35.mycashserver.service.UserService;
 
-@Controller
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
 public class AccountController {
+    private final UserService userService;
+    private final AccountService accountService;
+    private final AccountMapper accountMapper;
+
+    @GetMapping("/get")
+    public List<AccountDTO> getUserAccounts() {
+        User user = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        return accountService.getAllAccounts(user).stream().map(accountMapper::toAccountDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/get/{accountName}")
+    public AccountDTO getUserAccount(@PathVariable String accountName) {
+        User user = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        return accountMapper.toAccountDTO(accountService.getAccountByOwnerAndName(user, accountName));
+    }
+
+    @PostMapping("/add")
+    public void addUserAccount(@RequestBody AccountDTO accountDTO) {
+
+    }
 }
