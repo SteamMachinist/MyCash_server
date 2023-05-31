4c85f09f4ce6758661cfdef7b07bb382012ca475
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-01 01:42:16 +0300

    Добавлен маппинг листов объеков

--- src/main/java/tp35/mycashserver/mapper/CategoryMapper.java
+++ src/main/java/tp35/mycashserver/mapper/CategoryMapper.java
@@ -6,6 +6,8 @@ import tp35.mycashserver.dto.CategoryDTO;
 import tp35.mycashserver.entity.CategoryEntity;
 import tp35.mycashserver.model.Category;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",
         uses = {UserMapper.class, BaseCategoryMapper.class})
 public interface CategoryMapper {
@@ -16,4 +18,11 @@ public interface CategoryMapper {
     @Mapping(source = "baseCategory.name", target = "name")
     @Mapping(source = "baseCategory.type", target = "type")
     CategoryDTO toCategoryDTO(Category category);
+
+
+    List<CategoryEntity> toCategoryEntities(List<Category> category);
+
+    List<Category> toCategories(List<CategoryEntity> categoryEntities);
+
+    List<CategoryDTO> toCategoryDTOs(List<Category> category);
 }
