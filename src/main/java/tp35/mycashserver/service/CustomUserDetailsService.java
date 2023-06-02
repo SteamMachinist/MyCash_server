e250c8887496c6ef430ec6e34cf154e31ecb479c
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 23:53:17 +0300

    Разделены UserDetailsService и AuthenticationService

---
+++ src/main/java/tp35/mycashserver/service/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package tp35.mycashserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import tp35.mycashserver.model.JwtUserDetails;
+import tp35.mycashserver.model.User;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) {
+        User user = userService.getUserByUsername(username);
+        return new JwtUserDetails(user.getId(), username, user.getPassword(), user.getRoles());
+    }
+}
