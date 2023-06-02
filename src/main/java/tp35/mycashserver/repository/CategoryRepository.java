70c674e37504468c536ce106b04c21b5d89c2788
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 12:46:33 +0300

    Добавлен запрос на нахождение всех категорий пользователя

--- src/main/java/tp35/mycashserver/repository/CategoryRepository.java
+++ src/main/java/tp35/mycashserver/repository/CategoryRepository.java
@@ -2,6 +2,10 @@ package tp35.mycashserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tp35.mycashserver.entity.CategoryEntity;
+import tp35.mycashserver.entity.UserEntity;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    List<CategoryEntity> findAllByUser(UserEntity userEntity);
 }
