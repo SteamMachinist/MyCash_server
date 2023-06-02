5ebe26925bd4db9ca867dcdf41f07bd8f3f4e3be
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 12:49:48 +0300

    Начат crud категорий

--- src/main/java/tp35/mycashserver/controller/CategoryController.java
+++ src/main/java/tp35/mycashserver/controller/CategoryController.java
@@ -1,7 +1,32 @@
 package tp35.mycashserver.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import tp35.mycashserver.dto.CategoryDTO;
+import tp35.mycashserver.mapper.CategoryMapper;
+import tp35.mycashserver.model.User;
+import tp35.mycashserver.service.CategoryService;
+import tp35.mycashserver.service.UserService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+    private final UserService userService;
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping("/get_all")
+    public List<CategoryDTO> getAllUserCategories() {
+        User user = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        return categoryMapper.toCategoryDTOs(categoryService.getAllUserCategories(user));
+    }
+
+    @PostMapping("/update")
+    public void updateCategory(@RequestBody CategoryDTO categoryDTO) {
+
+    }
 }
--- src/main/java/tp35/mycashserver/service/CategoryService.java
+++ src/main/java/tp35/mycashserver/service/CategoryService.java
@@ -3,14 +3,25 @@ package tp35.mycashserver.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tp35.mycashserver.mapper.CategoryMapper;
+import tp35.mycashserver.mapper.UserMapper;
 import tp35.mycashserver.model.Category;
+import tp35.mycashserver.model.User;
 import tp35.mycashserver.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
+    private final UserMapper userMapper;
+
+    //public void addUserCategory()
+
+    public List<Category> getAllUserCategories(User user) {
+        return categoryMapper.toCategories(categoryRepository.findAllByUser(userMapper.toUserEntity(user)));
+    }
 
     public void addCategory(Category category) {
         categoryRepository.save(categoryMapper.toCategoryEntity(category));
