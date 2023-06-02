ddf5e040eea45925ae70c4873f7ac30da2cefbdb
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 23:58:52 +0300

    Реализован crud контроллер операций

--- src/main/java/tp35/mycashserver/controller/OperationController.java
+++ src/main/java/tp35/mycashserver/controller/OperationController.java
@@ -1,7 +1,40 @@
 package tp35.mycashserver.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tp35.mycashserver.dto.OperationDTO;
+import tp35.mycashserver.mapper.OperationMapper;
+import tp35.mycashserver.model.User;
+import tp35.mycashserver.service.AuthenticationService;
+import tp35.mycashserver.service.OperationService;
 
-@Controller
+@RestController
+@RequestMapping("/api/operation")
+@RequiredArgsConstructor
 public class OperationController {
+    private final AuthenticationService authenticationService;
+    private final OperationService operationService;
+    private final OperationMapper operationMapper;
+
+    @GetMapping("/get/{id}")
+    public OperationDTO getOperation(@PathVariable Long id) {
+        return operationMapper.toOperationDTO(operationService.getOperationById(id));
+    }
+
+    @PostMapping("/add")
+    public void addOperation(@RequestBody OperationDTO operationDTO) {
+        User user = authenticationService.getAuthenticatedUser();
+        operationService.addOperation(user, operationDTO);
+    }
+
+    @PostMapping("/update")
+    public void updateOperation(@RequestBody OperationDTO operationDTO) {
+        User user = authenticationService.getAuthenticatedUser();
+        operationService.updateOperation(user, operationDTO);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteOperation(@PathVariable Long id) {
+        operationService.deleteOperation(id);
+    }
 }
