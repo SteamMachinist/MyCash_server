9584a7ef356bd04711572cdda07cd34bf405caeb
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 23:53:17 +0300

    Разделены UserDetailsService и AuthenticationService

--- src/main/java/tp35/mycashserver/service/AuthenticationService.java
+++ src/main/java/tp35/mycashserver/service/AuthenticationService.java
@@ -10,14 +10,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import tp35.mycashserver.mapper.UserMapper;
 import tp35.mycashserver.model.Account;
 import tp35.mycashserver.model.JwtUserDetails;
 import tp35.mycashserver.model.Role;
 import tp35.mycashserver.model.User;
-import tp35.mycashserver.repository.UserRepository;
 import tp35.mycashserver.request.AuthenticationRequest;
 import tp35.mycashserver.request.FirstAccountRequest;
+import tp35.mycashserver.request.RegisterRequest;
 import tp35.mycashserver.response.TokenResponse;
 
 import java.util.Collections;
@@ -25,33 +24,27 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class AuthenticationService implements UserDetailsService {
-    //private final AuthenticationManager authenticationManager;
+public class AuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService userDetailsService;
     private final UserService userService;
+    private final CategoryService categoryService;
     private final AccountService accountService;
     private final JwtTokenService jwtTokenService;
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-
-    @Override
-    public UserDetails loadUserByUsername(final String username) {
-        User user = userService.getUserByUsername(username);
-        return new JwtUserDetails(user.getId(), username, user.getPassword(), user.getRoles());
-    }
 
     public User getAuthenticatedUser() {
         return userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
     }
 
-//    public User loginUser(AuthenticationRequest authenticationRequest) {
-//        try {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-//                    authenticationRequest.getUsername(), authenticationRequest.getPassword()));
-//        } catch (final BadCredentialsException ex) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-//        }
-//        return getAuthenticatedUser();
-//    }
+    public User loginUser(AuthenticationRequest authenticationRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (final BadCredentialsException ex) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        return getAuthenticatedUser();
+    }
 
     public User createNewUserFromRequest(FirstAccountRequest firstAccountRequest) {
         User user = new User(null, UUID.randomUUID().toString(), "", Collections.singleton(Role.UNREGISTERED));
@@ -62,11 +55,21 @@ public class AuthenticationService implements UserDetailsService {
                 firstAccountRequest.getAccountBalance(),
                 null, false, null);
         accountService.addAccount(account);
+        categoryService.addUserCategoriesFormBaseCategories(user);
         return user;
     }
 
     public TokenResponse getUserToken(User user) {
-        final UserDetails userDetails = loadUserByUsername(user.getUsername());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
         return new TokenResponse(jwtTokenService.generateToken(userDetails));
+    }
+
+    public User registerUser(RegisterRequest registerRequest) {
+        User user = getAuthenticatedUser();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setRoles(Collections.singleton(Role.REGISTERED));
+        userService.addUser(user);
+        return user;
     }
 }
