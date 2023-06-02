eb6a18f4e69edf255b529689dde622511bba2bec
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 23:54:45 +0300

    В маппер включены операции над листами

--- src/main/java/tp35/mycashserver/mapper/BaseCategoryMapper.java
+++ src/main/java/tp35/mycashserver/mapper/BaseCategoryMapper.java
@@ -4,8 +4,14 @@ import org.mapstruct.Mapper;
 import tp35.mycashserver.entity.BaseCategoryEntity;
 import tp35.mycashserver.model.BaseCategory;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface BaseCategoryMapper {
     BaseCategoryEntity toBaseCategoryEntity(BaseCategory baseCategory);
     BaseCategory toBaseCategory(BaseCategoryEntity baseCategoryEntity);
+
+    List<BaseCategoryEntity> toBaseCategoryEntities(List<BaseCategory> baseCategory);
+
+    List<BaseCategory> toBaseCategories(List<BaseCategoryEntity> baseCategoryEntities);
 }
