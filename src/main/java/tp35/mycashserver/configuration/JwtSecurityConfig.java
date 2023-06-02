b5bdbbcc7f66cd2870602ec3cfa5273e8d8fdc0e
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-02 12:32:35 +0300

    SecurityConfig разделен на 2 файла

--- src/main/java/tp35/mycashserver/configuration/JwtSecurityConfig.java
+++ src/main/java/tp35/mycashserver/configuration/JwtSecurityConfig.java
@@ -4,25 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import tp35.mycashserver.model.Role;
-import tp35.mycashserver.security.JwtRequestFilter;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
 public class JwtSecurityConfig {
-    private final JwtRequestFilter jwtRequestFilter;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -32,26 +20,5 @@ public class JwtSecurityConfig {
     public AuthenticationManager authenticationManager(
             final AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
-        return http
-                .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-
-                .authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-                                .requestMatchers("/api/auth/new", "/api/auth/login").permitAll()
-                                .requestMatchers("/api/auth/register").hasAuthority(Role.UNREGISTERED.getAuthority())
-                                .anyRequest().hasAnyAuthority(Role.UNREGISTERED.getAuthority(), Role.REGISTERED.getAuthority())
-                )
-
-                .sessionManagement(configurer ->
-                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
     }
 }
