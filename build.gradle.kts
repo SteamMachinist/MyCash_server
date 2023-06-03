c373fda6f92c9c8f93fa8bb031c36948ea982ec3
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-03 23:58:22 +0300

    Добавлена зависимость apache.commons:commons-math для реализации интерполяции

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+
+	implementation("org.apache.commons:commons-math3:3.6.1")
 }
 
 tasks.withType<Test> {
