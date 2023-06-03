ee732b347a4ba94a29ee5e662174050c8874fffa
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-04 02:06:19 +0300

    Добавлен объект ответа сервера о превышении лимитов при добавлении операции

--- src/main/java/tp35/mycashserver/response/OperationAddResponse.java
+++ src/main/java/tp35/mycashserver/response/OperationAddResponse.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class OperationAddResponse {
-    private final Boolean isOverLimit;
+    private final OverLimitType overLimitType;
 }
--- src/main/java/tp35/mycashserver/response/OverLimitType.java
+++ src/main/java/tp35/mycashserver/response/OverLimitType.java
@@ -1,6 +1,7 @@
 package tp35.mycashserver.response;
 
 public enum OverLimitType {
+    NONE,
     CATEGORY,
     ACCOUNT
 }
