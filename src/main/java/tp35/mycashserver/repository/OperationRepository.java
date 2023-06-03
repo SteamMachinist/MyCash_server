731b9439ae15058e41ae3cd5a38b52d71b91fa3b
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-03 23:59:42 +0300

    Добавлены запросы по категории и по типу категории

--- src/main/java/tp35/mycashserver/repository/OperationRepository.java
+++ src/main/java/tp35/mycashserver/repository/OperationRepository.java
@@ -2,7 +2,9 @@ package tp35.mycashserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tp35.mycashserver.entity.AccountEntity;
+import tp35.mycashserver.entity.CategoryEntity;
 import tp35.mycashserver.entity.OperationEntity;
+import tp35.mycashserver.model.CategoryType;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.List;
 public interface OperationRepository extends JpaRepository<OperationEntity, Long> {
     List<OperationEntity> findAllByAccount(AccountEntity account);
 
-    List<OperationEntity> findAllByAccountAndDateTimeBetween(AccountEntity account, LocalDateTime from, LocalDateTime to);
+    List<OperationEntity> findAllByAccountAndDateTimeBetween(AccountEntity accountEntity, LocalDateTime from, LocalDateTime to);
+
+    List<OperationEntity> findAllByAccountAndCategoryAndDateTimeBetween(AccountEntity accountEntity, CategoryEntity categoryEntity, LocalDateTime from, LocalDateTime to);
+
+    List<OperationEntity> findAllByAccountAndCategory_BaseCategory_TypeAndDateTimeBetween(AccountEntity accountEntity, CategoryType categoryType, LocalDateTime from, LocalDateTime to);
 }
