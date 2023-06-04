c83a2fa511fdf41dc67c71964a57266359d4f22a
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-04 11:00:31 +0300

    Изменено название контроллера аналитики

---
+++ src/main/java/tp35/mycashserver/controller/AnalyticsController.java
@@ -0,0 +1,26 @@
+package tp35.mycashserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/analytics")
+@RequiredArgsConstructor
+public class AnalyticsController {
+//    @GetMapping("/{accountName}/all")
+//    public void a1() {
+//
+//    }
+//
+//    @GetMapping("/{accountName}/income")
+//    public void a1() {
+//
+//    }
+//
+//    @GetMapping("/{accountName}/income")
+//    public void a1() {
+//
+//    }
+}
