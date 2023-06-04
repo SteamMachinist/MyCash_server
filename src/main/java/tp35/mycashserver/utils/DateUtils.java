404754159a5efc2bf0219d0b3ff2502f960aa848
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-04 15:05:00 +0300

    Расширена утилита получения дат

--- src/main/java/tp35/mycashserver/utils/DateUtils.java
+++ src/main/java/tp35/mycashserver/utils/DateUtils.java
@@ -1,7 +1,5 @@
 package tp35.mycashserver.utils;
 
-import org.apache.commons.math3.util.Pair;
-
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
@@ -9,7 +7,7 @@ import java.time.temporal.TemporalAdjusters;
 
 public class DateUtils {
     public static Pair<LocalDateTime, LocalDateTime> getDatesBorderMonthDays(int year1, int month1, int day1,
-                                                                      int year2, int month2, int day2) {
+                                                                             int year2, int month2, int day2) {
         LocalDateTime base = LocalDateTime.now();
         LocalDateTime from = base.toLocalDate()
                 .withYear(year1)
@@ -26,7 +24,7 @@ public class DateUtils {
     }
 
     public static Pair<LocalDateTime, LocalDateTime> getDatesBorderMonth(int year1, int month1,
-                                                                  int year2, int month2) {
+                                                                         int year2, int month2) {
         LocalDateTime base = LocalDateTime.now();
         LocalDateTime from = base.toLocalDate()
                 .withYear(year1)
@@ -37,6 +35,22 @@ public class DateUtils {
         LocalDateTime to = base.toLocalDate()
                 .withYear(year2)
                 .withMonth(month2)
+                .with(TemporalAdjusters.lastDayOfMonth())
+                .atTime(LocalTime.MAX).minus(1, ChronoUnit.SECONDS);
+        return new Pair<>(from, to);
+    }
+
+    public static Pair<LocalDateTime, LocalDateTime> getDatesBorderYear(int year1, int year2) {
+        LocalDateTime base = LocalDateTime.now();
+        LocalDateTime from = base.toLocalDate()
+                .withYear(year1)
+                .withMonth(1)
+                .with(TemporalAdjusters.firstDayOfMonth())
+                .atTime(LocalTime.MIN);
+
+        LocalDateTime to = base.toLocalDate()
+                .withYear(year2)
+                .withMonth(12)
                 .with(TemporalAdjusters.lastDayOfMonth())
                 .atTime(LocalTime.MAX).minus(1, ChronoUnit.SECONDS);
         return new Pair<>(from, to);
