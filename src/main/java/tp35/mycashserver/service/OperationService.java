19643201ca2bc1c4e69ad155e4b1c4bd9325de84
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-04 15:08:52 +0300

    Реализовано изменение баланса при добавлении и изменении операций

--- src/main/java/tp35/mycashserver/service/OperationService.java
+++ src/main/java/tp35/mycashserver/service/OperationService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tp35.mycashserver.dto.OperationDTO;
 import tp35.mycashserver.mapper.OperationMapper;
+import tp35.mycashserver.model.Account;
+import tp35.mycashserver.model.CategoryType;
 import tp35.mycashserver.model.Operation;
 import tp35.mycashserver.model.User;
 import tp35.mycashserver.repository.OperationRepository;
@@ -20,8 +22,16 @@ public class OperationService {
     private final AccountService accountService;
 
     public Operation setUpOperationForUserFromDto(User user, Operation operation, OperationDTO operationDTO) {
+        return setUpOperationForUserFromDto(
+                user,
+                accountService.getAccountByOwnerAndName(user, operationDTO.getAccountName()),
+                operation,
+                operationDTO);
+    }
+
+    public Operation setUpOperationForUserFromDto(User user, Account account, Operation operation, OperationDTO operationDTO) {
         operation.setCategory(categoryService.getCategoryByUserAndName(user, operationDTO.getCategory().getName()));
-        operation.setAccount(accountService.getAccountByOwnerAndName(user, operationDTO.getAccountName()));
+        operation.setAccount(account);
         operation.setValue(operationDTO.getValue());
         operation.setDateTime(LocalDateTime.parse(operationDTO.getDateTime()));
         operation.setComment(operationDTO.getComment());
@@ -33,6 +43,14 @@ public class OperationService {
     }
 
     public void addOperation(User user, OperationDTO operationDTO) {
+        Account account = accountService.getAccountByOwnerAndName(user, operationDTO.getAccountName());
+
+        CategoryType type = operationDTO.getCategory().getType();
+        double signedValue = type == CategoryType.EXPENSE ? -operationDTO.getValue() : operationDTO.getValue();
+        account.setBalance(account.getBalance() + signedValue);
+
+        accountService.addAccount(account);
+
         Operation operation = new Operation();
         addOperation(setUpOperationForUserFromDto(user, operation, operationDTO));
     }
@@ -42,7 +60,19 @@ public class OperationService {
     }
 
     public void updateOperation(User user, OperationDTO operationDTO) {
+        Account account = accountService.getAccountByOwnerAndName(user, operationDTO.getAccountName());
         Operation operation = getOperationById(operationDTO.getId());
+
+        CategoryType type = operation.getCategory().getBaseCategory().getType();
+        double signedValue = type == CategoryType.EXPENSE ? -operation.getValue() : operation.getValue();
+        account.setBalance(account.getBalance() - signedValue);
+
+        type = operationDTO.getCategory().getType();
+        signedValue = type == CategoryType.EXPENSE ? -operationDTO.getValue() : operationDTO.getValue();
+        account.setBalance(account.getBalance() + signedValue);
+
+        accountService.addAccount(account);
+
         addOperation(setUpOperationForUserFromDto(user, operation, operationDTO));
     }
 
