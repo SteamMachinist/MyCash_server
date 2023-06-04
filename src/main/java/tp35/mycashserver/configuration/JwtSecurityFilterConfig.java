c1969291eebc30e788ef926b4e13235cc00dac27
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-05 01:40:22 +0300

    Изменена конфигурация spring security для контроллера администратора

--- src/main/java/tp35/mycashserver/configuration/JwtSecurityFilterConfig.java
+++ src/main/java/tp35/mycashserver/configuration/JwtSecurityFilterConfig.java
@@ -1,37 +1,48 @@
 package tp35.mycashserver.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import tp35.mycashserver.model.Role;
+import tp35.mycashserver.service.CustomUserDetailsService;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class JwtSecurityFilterConfig {
     private final JwtRequestFilter jwtRequestFilter;
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
 
     @Bean
     public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
         return http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
                                 .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                                 .requestMatchers("/api/auth/new", "/api/auth/login").permitAll()
-                                .requestMatchers("/api/auth/register").hasAuthority(Role.UNREGISTERED.getAuthority())
-                                .anyRequest().hasAnyAuthority(Role.UNREGISTERED.getAuthority(), Role.REGISTERED.getAuthority())
-                )
+                                .requestMatchers("/api/auth/register").hasAnyAuthority(Role.UNREGISTERED.getAuthority(), Role.ADMIN.getAuthority())
+                                .requestMatchers("/api/**").hasAnyAuthority(Role.UNREGISTERED.getAuthority(), Role.REGISTERED.getAuthority(), Role.ADMIN.getAuthority())
+                                .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority()))
 
                 .sessionManagement(configurer ->
                         configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
