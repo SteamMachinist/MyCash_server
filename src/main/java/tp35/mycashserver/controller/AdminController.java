e016cc9d487b2b4031d88c6c8da62067a44c265f
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-05 01:40:55 +0300

    Обозначен контроллер администратора

--- src/main/java/tp35/mycashserver/controller/AdminController.java
+++ src/main/java/tp35/mycashserver/controller/AdminController.java
@@ -1,7 +1,17 @@
 package tp35.mycashserver.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping
+    public String example() {
+        return "hello";
+    }
 }
