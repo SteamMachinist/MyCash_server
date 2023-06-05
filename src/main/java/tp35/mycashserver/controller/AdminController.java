8a125a54077ea21cdafa969cae8ac31078c07060
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-05 17:43:59 +0300

    Swagger контроллера администратора

--- src/main/java/tp35/mycashserver/controller/AdminController.java
+++ src/main/java/tp35/mycashserver/controller/AdminController.java
@@ -1,5 +1,7 @@
 package tp35.mycashserver.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,22 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import tp35.mycashserver.service.AdminService;
 
 @Controller
+@Tag(name = "Admin", description = "Admin features")
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
 
+    @Operation(summary = "Admin panel index")
     @GetMapping
     public String index() {
         return "admin_index";
     }
 
+    @Operation(summary = "User statistics")
     @GetMapping("/users_stats")
     public String usersStats(Model model) {
         model.addAllAttributes(adminService.getUsersStats());
         return "users_stats";
     }
 
+    @Operation(summary = "Category statistics")
     @GetMapping("/category_stats")
     public String categoryStats(Model model) {
         var cat = adminService.getCategoriesStats();
