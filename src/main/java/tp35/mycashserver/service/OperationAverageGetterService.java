455de71081b3571d935f10dc9ee83e527b200543
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-13 19:37:53 +0300

    Изменены сервисы получения суммы и среднего операций для использования новых кастомынх запросов

--- src/main/java/tp35/mycashserver/service/OperationAverageGetterService.java
+++ src/main/java/tp35/mycashserver/service/OperationAverageGetterService.java
@@ -2,16 +2,23 @@ package tp35.mycashserver.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tp35.mycashserver.mapper.BaseCategoryMapper;
 import tp35.mycashserver.model.BaseCategory;
-import tp35.mycashserver.model.Operation;
+import tp35.mycashserver.repository.OperationRepository;
+import tp35.mycashserver.utils.DateUtils;
+import tp35.mycashserver.utils.Pair;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class OperationAverageGetterService {
-    private final OperationsGetterService operationsGetterService;
+    private final OperationRepository operationRepository;
+    private final BaseCategoryMapper baseCategoryMapper;
 
-    public Double getAverageOperationsByBaseCategoryByDate(BaseCategory baseCategory, int year, int month) {
-        return operationsGetterService.getOperationsByBaseCategoryByDate(baseCategory, year, month).stream()
-                .mapToDouble(Operation::getValue).average().orElse(0.0);
+    public Double getOperationsAverageBy(BaseCategory baseCategory, int year, int month) {
+        Pair<LocalDateTime, LocalDateTime> monthDates = DateUtils.getBorderingDatesBetween(year, month, year, month);
+        return operationRepository.averageOfOperationsBy(baseCategoryMapper.toBaseCategoryEntity(baseCategory),
+                monthDates.getFirst(), monthDates.getSecond()).orElse(0.0);
     }
 }
