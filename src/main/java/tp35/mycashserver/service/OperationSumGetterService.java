68784fca3781fe776d5a4d821f32cb42e822f55c
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-13 19:41:51 +0300

    Сервис суммы исправлен на использование новых запросов

--- src/main/java/tp35/mycashserver/service/OperationSumGetterService.java
+++ src/main/java/tp35/mycashserver/service/OperationSumGetterService.java
@@ -2,68 +2,70 @@ package tp35.mycashserver.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tp35.mycashserver.mapper.AccountMapper;
+import tp35.mycashserver.mapper.CategoryMapper;
 import tp35.mycashserver.model.Account;
 import tp35.mycashserver.model.Category;
 import tp35.mycashserver.model.CategoryType;
-import tp35.mycashserver.model.Operation;
+import tp35.mycashserver.repository.OperationRepository;
+import tp35.mycashserver.utils.DateUtils;
+import tp35.mycashserver.utils.Pair;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OperationSumGetterService {
-    private final OperationsGetterService operationsGetterService;
+    private final OperationRepository operationRepository;
+    private final AccountMapper accountMapper;
+    private final CategoryMapper categoryMapper;
 
-    public Double getSumOperationsByAccountByDate(Account account, int year) {
-        return operationsGetterService.getOperationsByAccountByDate(account, year).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, int year, int month) {
+        Pair<LocalDateTime, LocalDateTime> monthDates = DateUtils.getBorderingDatesBetween(year, month, year, month);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account), monthDates.getFirst(), monthDates.getSecond())
+                .orElse(0.0);
     }
 
-    public Double getSumOperationsByAccountByCategoryByDate(Account account, Category category, int year) {
-        return operationsGetterService.getOperationsByAccountByCategoryByDate(account, category, year).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, int year, int month, int day) {
+        Pair<LocalDateTime, LocalDateTime> dayDates = DateUtils.getBorderingDatesBetween(year, month, day, year, month, day);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account), dayDates.getFirst(), dayDates.getSecond())
+                .orElse(0.0);
     }
 
-    public Double getSumOperationsByAccountByCategoryTypeByDate(Account account, CategoryType categoryType, int year) {
-        return operationsGetterService.getOperationsByAccountByCategoryTypeByDate(account, categoryType, year).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, Category category, int year, int month) {
+        Pair<LocalDateTime, LocalDateTime> monthDates = DateUtils.getBorderingDatesBetween(year, month, year, month);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account),
+                categoryMapper.toCategoryEntity(category), monthDates.getFirst(), monthDates.getSecond())
+                .orElse(0.0);
     }
 
-    public Double getSumOperationsByAccountByDate(Account account, int year, int month) {
-        return operationsGetterService.getOperationsByAccountByDate(account, year, month).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, Category category, int year, int month, int day) {
+        Pair<LocalDateTime, LocalDateTime> dayDates = DateUtils.getBorderingDatesBetween(year, month, day, year, month, day);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account),
+                categoryMapper.toCategoryEntity(category), dayDates.getFirst(), dayDates.getSecond())
+                .orElse(0.0);
     }
 
-    public Double getSumOperationsByAccountByCategoryByDate(Account account, Category category, int year, int month) {
-        return operationsGetterService.getOperationsByAccountByCategoryByDate(account, category, year, month).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, CategoryType categoryType, int year, int month) {
+        Pair<LocalDateTime, LocalDateTime> monthDates = DateUtils.getBorderingDatesBetween(year, month, year, month);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account),
+                categoryType, monthDates.getFirst(), monthDates.getSecond())
+                .orElse(0.0);
     }
 
-    public Double getSumOperationsByAccountByCategoryTypeByDate(Account account, CategoryType categoryType, int year, int month) {
-        return operationsGetterService.getOperationsByAccountByCategoryTypeByDate(account, categoryType, year, month).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
-    }
-
-    public Double getSumOperationsByAccountByDate(Account account, int year, int month, int day) {
-        return operationsGetterService.getOperationsByAccountByDate(account, year, month, day).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
-    }
-
-    public Double getSumOperationsByAccountByCategoryByDate(Account account, Category category, int year, int month, int day) {
-        return operationsGetterService.getOperationsByAccountByCategoryByDate(account, category, year, month, day).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
-    }
-
-    public Double getSumOperationsByAccountByCategoryTypeByDate(Account account, CategoryType categoryType, int year, int month, int day) {
-        return operationsGetterService.getOperationsByAccountByCategoryTypeByDate(account, categoryType, year, month, day).stream()
-                .map(Operation::getValue).reduce(Double::sum).orElse(0.0);
+    public double getOperationSumBy(Account account, CategoryType categoryType, int year, int month, int day) {
+        Pair<LocalDateTime, LocalDateTime> dayDates = DateUtils.getBorderingDatesBetween(year, month, day, year, month, day);
+        return operationRepository.sumOfOperationsBy(accountMapper.toAccountEntity(account),
+                categoryType, dayDates.getFirst(), dayDates.getSecond())
+                .orElse(0.0);
     }
 
     public List<Double> getSumByTypeByMonthList(List<LocalDate> months, Account account, CategoryType categoryType) {
         return months.stream()
                 .map(localDate ->
-                        getSumOperationsByAccountByCategoryTypeByDate(
+                        getOperationSumBy(
                                 account,
                                 categoryType,
                                 localDate.getYear(),
