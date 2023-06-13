a861ab25161c63a2668b2cbb6053ee32961e4304
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-06-13 19:41:10 +0300

    Исправлено на использование нового сервиса суммы

--- src/main/java/tp35/mycashserver/service/LimitCheckService.java
+++ src/main/java/tp35/mycashserver/service/LimitCheckService.java
@@ -2,6 +2,7 @@ package tp35.mycashserver.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tp35.mycashserver.model.CategoryType;
 import tp35.mycashserver.model.Operation;
 import tp35.mycashserver.response.OperationAddResponse;
 import tp35.mycashserver.response.OverLimitType;
@@ -12,9 +13,13 @@ public class LimitCheckService {
     private final OperationSumGetterService operationSumGetterService;
 
     public OperationAddResponse checkLimit(Operation operation) {
+        if (operation.getCategory().getBaseCategory().getType() == CategoryType.INCOME) {
+            return new OperationAddResponse(OverLimitType.NONE);
+        }
+
         if(operation.getAccount().getIsLimited()) {
             double limit = operation.getAccount().getSpendingLimit();
-            double spending = operationSumGetterService.getSumOperationsByAccountByDate(
+            double spending = operationSumGetterService.getOperationSumBy(
                     operation.getAccount(),
                     operation.getDateTime().getYear(),
                     operation.getDateTime().getMonthValue());
@@ -22,9 +27,10 @@ public class LimitCheckService {
                 return new OperationAddResponse(OverLimitType.ACCOUNT);
             }
         }
+
         if (operation.getCategory().getIsLimited()) {
             double limit = operation.getCategory().getSpendingLimit();
-            double spending = operationSumGetterService.getSumOperationsByAccountByCategoryByDate(
+            double spending = operationSumGetterService.getOperationSumBy(
                     operation.getAccount(),
                     operation.getCategory(),
                     operation.getDateTime().getYear(),
